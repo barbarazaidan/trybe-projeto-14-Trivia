e98bef9e42d174c0c4971655ad83f2d082dd999f
rosana-carmona <rocarmona2010@hotmail.com>
2023-02-09 16:09:15 -0300

    Requisito 1

---
+++ src/redux/actions/index.js
@@ -0,0 +1,11 @@
+export const ADD_NAME = 'ADD_NAME';
+export const addName = (name) => ({
+  type: 'ADD_NAME',
+  payload: name,
+});
+
+export const ADD_EMAIL = 'ADD_EMAIL';
+export const addEmail = (email) => ({
+  type: 'ADD_EMAIL',
+  payload: email,
+});
