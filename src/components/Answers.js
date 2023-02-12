d4690fbe280897d71e4e258f03045dfb0c3a4c09
Bárbara <babivaldez@gmail.com>
2023-02-12 01:10:07 -0300

    refatora o requisito 6

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -6,9 +6,10 @@ class Answers extends Component {
   state = {
     contador: 0,
     temporizador: 30,
-    // shuffled: [],
-    // currentQuestionResults: {},
-    // color: '',
+    shuffledAnswers: [],
+    correctAnswer: '',
+    category: '',
+    question: '',
     isClicked: false,
     isButtonDisabled: false,
     intervalId: 0,
@@ -30,6 +31,8 @@ class Answers extends Component {
     const maxTime = 30000;
     const tempInterval = 1000;
 
+    this.generateAnswers();
+
     setTimeout(this.timer, maxTime);
     const intervalIdi = setInterval(this.timerUser, tempInterval);
     this.setState({ intervalId: intervalIdi });
@@ -39,9 +42,10 @@ class Answers extends Component {
     const maxTime = 30000;
     const tempInterval = 1000;
 
-    console.log('Prevstate:', prevState);
+    // console.log('Prevstate:', prevState);
     const { contador } = this.state;
     if (contador > prevState.contador) {
+      this.generateAnswers();
       this.setState({
         isButtonDisabled: false,
         temporizador: 30,
@@ -51,6 +55,44 @@ class Answers extends Component {
       this.setState({ intervalId: intervalIdi });
     }
   }
+
+  generateAnswers = () => {
+    const { contador } = this.state;
+    const { questionResults } = this.props;
+    const {
+      incorrect_answers: incorrectAnswersAPI,
+      correct_answer: correctAnswerAPI,
+      category,
+      question,
+    } = questionResults[contador];
+
+    const answersOptions = [...incorrectAnswersAPI, correctAnswerAPI];
+    // console.log('currentAnswersOptions', answersOptions);
+
+    const shuffledAnswersOptions = this.shuffleAnswers(answersOptions);
+    // console.log(shuffledAnswers);
+
+    this.setState({
+      shuffledAnswers: shuffledAnswersOptions,
+      correctAnswer: correctAnswerAPI,
+      question,
+      category,
+    });
+  };
+
+  shuffleAnswers = (answersOptions) => {
+    const limitToSort = 0.5;
+    const shuffleAnswers = answersOptions.sort(() => Math.random() - limitToSort);
+    // console.log(shuffleAnswers);
+    return shuffleAnswers;
+  };
+
+  timer = () => {
+    this.setState({
+      isButtonDisabled: true,
+    });
+    console.log('oi');
+  };
 
   timerUser = () => {
     const { temporizador, intervalId } = this.state;
@@ -66,25 +108,6 @@ class Answers extends Component {
     this.setState(({ contador }) => ({ contador: contador + 1, isClicked: false }));
   };
 
-  shuffleAnswers = (currentAnswersOptions) => {
-    const limitToSort = 0.5;
-    const shuffleAnswers = currentAnswersOptions.sort(() => Math.random() - limitToSort);
-    // console.log(shuffleAnswers);
-    return shuffleAnswers;
-  };
-
-  // validateColor = (answer, correctAnswerAPI) => {
-  //   this.setState({ isOnClick: true });
-  // };
-
-  // validateColor = (answer, correctAnswerAPI) => {
-  //   if (answer === correctAnswerAPI) {
-  //     this.setState({ color: 'btnGreen' });
-  //   } else {
-  //     this.setState({ color: 'btnRed' });
-  //   }
-  // };
-
   validateColor = (answer, correctAnswerAPI) => {
     if (answer === correctAnswerAPI) {
       return 'btnGreen';
@@ -95,82 +118,38 @@ class Answers extends Component {
     this.setState({ isClicked: true });
   };
 
-  timer = () => {
-    this.setState({
-      isButtonDisabled: true,
-    });
-    console.log('oi');
-  };
-
-  answersOptions = (currentQuestionResults) => {
-    console.log('currentQuestionResults', currentQuestionResults);
-    const {
-      incorrect_answers: incorrectAnswersAPI, correct_answer: correctAnswerAPI,
-    } = currentQuestionResults;
-
-    const currentAnswersOptions = [...incorrectAnswersAPI, correctAnswerAPI];
-    console.log('currentAnswersOptions', currentAnswersOptions);
-
-    const shuffledAnswers = this.shuffleAnswers(currentAnswersOptions);
-    console.log(shuffledAnswers);
-
-    const { isClicked, isButtonDisabled } = this.state;
-
-    // this.setState({ shuffled: shuffledAnswers, currentQuestionResults });
-
-    return (
-      <div data-testid="answer-options">
-        { shuffledAnswers.map((answer, index) => (
-          <button
-            type="button"
-            key={ index }
-            data-testid={
-              answer === correctAnswerAPI ? 'correct-answer' : `wrong-answer-${index}`
-            }
-            // className={ isOnClick && answer === correctAnswerAPI ? 'bntGreen' : 'bntRed' }
-            className={ isClicked ? this.validateColor(answer, correctAnswerAPI) : '' }
-            // onClick={ () => this.validateColor(answer, correctAnswerAPI) }
-            onClick={ this.isClickedBtn }
-            disabled={ isButtonDisabled }
-            // disabled={ setTimeout(this.timer, 5000) }
-          >
-            { answer }
-          </button>
-        ))}
-        {/* { setTimeout(this.timer, 5000) } */}
-      </div>
-    );
-  };
-
   render() {
-    const { questionResults } = this.props;
-    const { contador, temporizador } = this.state;
-    // const { contador, shuffled, currentQuestionResults } = this.state;
-    // const {
-    //   incorrect_answers: incorrectAnswersAPI, correct_answer: correctAnswerAPI,
-    // } = currentQuestionResults;
+    const {
+      category,
+      question,
+      temporizador,
+      isClicked,
+      isButtonDisabled,
+      shuffledAnswers,
+      correctAnswer,
+    } = this.state;
 
     return (
       <div>
         <p id="temporizador">{ temporizador }</p>
-        <p data-testid="question-category">{ questionResults[contador].category }</p>
-        <p data-testid="question-text">{ questionResults[contador].question }</p>
-        { this.answersOptions(questionResults[contador]) }
-        {/* <div data-testid="answer-options">
-          { shuffled.map((answer, index) => (
+        <p data-testid="question-category">{ category }</p>
+        <p data-testid="question-text">{ question }</p>
+        <div data-testid="answer-options">
+          { shuffledAnswers.map((answer, index) => (
             <button
               type="button"
               key={ index }
               data-testid={
-                answer === correctAnswerAPI ? 'correct-answer' : `wrong-answer-${index}`
+                answer === correctAnswer ? 'correct-answer' : `wrong-answer-${index}`
               }
-              // className={ isOnClick && answer === correctAnswerAPI ? 'bntGreen' : 'bntRed' }
-              onClick={ this.validateColor }
+              className={ isClicked ? this.validateColor(answer, correctAnswer) : '' }
+              onClick={ this.isClickedBtn }
+              disabled={ isButtonDisabled }
             >
               { answer }
             </button>
           ))}
-        </div> */}
+        </div>
         <button
           type="button"
           data-testid="btn-next"
