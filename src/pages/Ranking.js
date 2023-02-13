4ae7fe4a7e5e76b1e721256fb52aa4137914e6f3
rosana-carmona <rocarmona2010@hotmail.com>
2023-02-12 23:35:16 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Ranking extends React.Component {
+class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking!</h1>
+        <div>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ () => history.push('/') }
+          >
+            Home
+          </button>
+        </div>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
+
+export default connect()(Ranking);
